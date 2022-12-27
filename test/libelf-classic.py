d7f12df345271c2afaf122b5dc6c0ead69d3d672
Sonal Santan <sonal.santan@amd.com>
2022-12-26 19:47:06 -0800

    Stop importing all symbols from elf and libelf

--- test/libelf-classic.py
+++ test/libelf-classic.py
@@ -10,8 +10,10 @@
 
 import sys
 import argparse
-from elf import *
-from libelf import *
+import ctypes
+
+import elf
+import libelf
 
 def parseCommandLine(args):
     msg = "Write out a sample ELF file"
@@ -21,12 +23,12 @@ def parseCommandLine(args):
     return parser.parse_args(args[1:])
 
 def writeELF(filename):
-    melf = ElfDescriptor.fromfile(filename, Elf_Cmd.ELF_C_WRITE)
+    melf = libelf.ElfDescriptor.fromfile(filename, libelf.Elf_Cmd.ELF_C_WRITE)
     ehdr = melf.elf32_newehdr()
 
-    ehdr.contents.e_ident[EI_DATA] = ELFDATA2MSB
-    ehdr.contents.e_machine = EM_PPC
-    ehdr.contents.e_type = ET_EXEC
+    ehdr.contents.e_ident[elf.EI_DATA] = elf.ELFDATA2MSB
+    ehdr.contents.e_machine = elf.EM_PPC
+    ehdr.contents.e_type = elf.ET_EXEC
 
     phdr = melf.elf32_newphdr(1)
 
@@ -37,15 +39,15 @@ def writeELF(filename):
     data.contents.d_align = 4
     data.contents.d_off = 0
     data.contents.d_buf = ctypes.cast(hash_words, ctypes.c_void_p)
-    data.contents.d_type = Elf_Type.ELF_T_WORD
+    data.contents.d_type = libelf.Elf_Type.ELF_T_WORD
     data.contents.d_size = ctypes.sizeof(hash_words)
-    data.contents.d_type = Elf_Type.ELF_T_BYTE
-    data.contents.d_version = EV_CURRENT
+    data.contents.d_type = libelf.Elf_Type.ELF_T_BYTE
+    data.contents.d_version = elf.EV_CURRENT
 
     shdr = scn.elf32_getshdr()
     shdr.contents.sh_name = 1
-    shdr.contents.sh_type = SHT_HASH
-    shdr.contents.sh_flags = SHF_ALLOC
+    shdr.contents.sh_type = elf.SHT_HASH
+    shdr.contents.sh_flags = elf.SHF_ALLOC
     shdr.contents.sh_entsize = 0
 
     scn2 = melf.elf_newscn()
@@ -58,25 +60,25 @@ def writeELF(filename):
     data2.contents.d_buf = ctypes.cast(string_table, ctypes.c_void_p)
     data2.contents.d_off = 0
     data2.contents.d_size = ctypes.sizeof(string_table)
-    data2.contents.d_type = Elf_Type.ELF_T_BYTE
-    data2.contents.d_version = EV_CURRENT
+    data2.contents.d_type = libelf.Elf_Type.ELF_T_BYTE
+    data2.contents.d_version = elf.EV_CURRENT
 
     shdr2 = scn2.elf32_getshdr()
     shdr2.contents.sh_name = 6
-    shdr2.contents.sh_type = SHT_STRTAB
-    shdr2.contents.sh_flags = SHF_STRINGS | SHF_ALLOC
+    shdr2.contents.sh_type = elf.SHT_STRTAB
+    shdr2.contents.sh_flags = elf.SHF_STRINGS | elf.SHF_ALLOC
     shdr2.contents.sh_entsize = 0
 
 
     ehdr.contents.e_shstrndx = scn2.elf_ndxscn()
 
-    melf.elf_update(Elf_Cmd.ELF_C_NULL)
+    melf.elf_update(libelf.Elf_Cmd.ELF_C_NULL)
 
-    phdr.contents.p_type = PT_PHDR
+    phdr.contents.p_type = elf.PT_PHDR
     phdr.contents.p_offset = ehdr.contents.e_phoff
-    phdr.contents.p_filesz = elf32_fsize(Elf_Type.ELF_T_PHDR, 1, EV_CURRENT)
-    melf.elf_flagphdr(Elf_Cmd.ELF_C_SET , ELF_F_DIRTY)
-    melf.elf_update(Elf_Cmd.ELF_C_WRITE)
+    phdr.contents.p_filesz = libelf.elf32_fsize(libelf.Elf_Type.ELF_T_PHDR, 1, elf.EV_CURRENT)
+    melf.elf_flagphdr(libelf.Elf_Cmd.ELF_C_SET , libelf.ELF_F_DIRTY)
+    melf.elf_update(libelf.Elf_Cmd.ELF_C_WRITE)
 
     del melf
 
